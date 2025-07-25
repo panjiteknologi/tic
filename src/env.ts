f203f1449ac6e4611e8ab30aaf2348e2909006e7
panjiggm <panjiggm@gmail.com>
2025-07-25 18:04:17 +0700

    setup database tic for auth first

--- src/env.ts
+++ src/env.ts
@@ -4,21 +4,20 @@ import { z } from "zod";
 export const env = createEnv({
   server: {
     NODE_ENV: z.enum(["development", "production"]),
-    NEXTAUTH_SECRET: z.string().min(1),
-    NEXTAUTH_URL: z.string().url(),
+    BETTER_AUTH_SECRET: z.string().min(1),
+    BETTER_AUTH_URL: z.string().url(),
+    DATABASE_URL: z.string().url(),
   },
 
   client: {
     // Client-side environment variables (if any) go here
-    NEXT_PUBLIC_ENDPOINT_URL: z.string().url(),
   },
 
   // Include all environment variables here
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-
-    NEXT_PUBLIC_ENDPOINT_URL: process.env.NEXT_PUBLIC_ENDPOINT_URL,
+    BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
+    BETTER_AUTH_URL: process.env.BETTER_AUTH_URL,
+    DATABASE_URL: process.env.DATABASE_URL,
   },
 });
